3b6b0eb48ef89efc68e3cc3f3134b39b2f7c57b1
emzamike1997-lab <emzamike1997@gmail.com>
2026-03-16 12:24:55 +0100

    JAVAS: Add Footer.js

---
+++ Footer.js
@@ -0,0 +1,15 @@
+```javascript
+import React from 'react';
+
+function Footer() {
+    return (
+        <div className="footer">
+            <p>&copy; 2024 Anime Website</p>
+        </div>
+    );
+}
+
+export default Footer;
+```
+
+Note: This code assumes you have a backend API that provides anime data. You'll need to replace the `https://api.example.com/animes` URL with your actual API endpoint. Additionally, you may need to modify the code to fit your specific requirements.
